5eeec6fd05bdd077626df89934eb676761987c8a
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-09-01 14:22:03 -0500

    Initial Commit

---
+++ TipJar/TipJarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipJarApp.swift
+//  TipJar
+//
+//  Created by Renato Oliveira Fraga on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TipJarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
